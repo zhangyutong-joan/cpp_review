e4e696f4e337111e081ae1f3b3222ddc5496ffb4
zhangyutong-joan <77472591+zhangyutong-joan@users.noreply.github.com>
2024-05-22 16:09:16 +0800

    map.count()函数

--- unordered_mapFunction.cpp
+++ unordered_mapFunction.cpp
@@ -12,7 +12,16 @@ int main(int argc, char const *argv[])
     //值传递遍历
     for(pair<int,int> kv:map){
             cout<<kv.first<<kv.second<<endl;
-        }
+    }
+    //判断key是否存在用count函数或者find
+    cout<<"---------------------------"<<endl;
+    int key=2;
+    if(map.count(key)){
+        cout<<key<<"是map的其中一个键"<<endl;
+    }else{
+        cout<<"map中没有值为"<<key<<"的键"<<endl;
+    }
+
 
     return 0;
 }
